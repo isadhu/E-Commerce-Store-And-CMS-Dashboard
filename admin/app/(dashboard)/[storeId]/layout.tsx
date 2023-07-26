af4d24fc2c738c41bc8e3b31f062460b89617458
isadhu <sadhu@Sadhus-MacBook-Pro.local>
2023-07-26 01:06:33 -0700

    Navbar & Settings Page

--- admin/app/(dashboard)/[storeId]/layout.tsx
+++ admin/app/(dashboard)/[storeId]/layout.tsx
@@ -1,8 +1,8 @@
 import { redirect } from 'next/navigation';
 import { auth } from '@clerk/nextjs';
 
-//import Navbar from '@/components/navbar';
 import prismadb from '@/lib/prismadb';
+import Navbar from '@/components/navbar/Navbar';
 
 export default async function DashboardLayout({
 	children,
@@ -30,8 +30,7 @@ export default async function DashboardLayout({
 
 	return (
 		<>
-			{/* <Navbar /> */}
-            <div> This will be a navbar</div>
+			<Navbar />
 			{children}
 		</>
 	);
